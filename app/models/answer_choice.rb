5640913dc6731f3154ef0fc07b49afdd12942364
Jared Planter <jaredplanter@gmail.com>
2017-04-12 12:36:59 -0700

    Add AnswerChoice#create_suggestion
    To create an associated AnswerChoiceSuggestion

--- app/models/answer_choice.rb
+++ app/models/answer_choice.rb
@@ -2,4 +2,8 @@ class AnswerChoice < ApplicationRecord
   belongs_to :question, required: false
   belongs_to :question_revision, required: false
   has_many :suggestions, class_name: "AnswerChoiceSuggestion"
+
+  def create_suggestion name
+    suggestions.create name: name
+  end
 end
