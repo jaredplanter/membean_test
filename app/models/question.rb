3e2b9b2c613c05a8cc9e1dc92068e2f5ef50d66a
Jared Planter <jaredplanter@gmail.com>
2017-04-12 11:31:43 -0700

    Update Question
    Create four answer choices when a question is created, letters A-D
    Question#answer is the answer choice with a true answer attribute

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,13 @@
 class Question < ApplicationRecord
   has_many :answer_choices
+
+  after_create do
+    ["A", "B", "C", "D"].each do |letter|
+      answer_choices.create letter: letter
+    end
+  end
+
+  def answer
+    answer_choices.find_by_answer true
+  end
 end
