74c47c080e98adc2978908c5f9b1aecb78749159
Jared Planter <jaredplanter@gmail.com>
2017-04-12 11:53:01 -0700

    Remove create callback from Question
    We do not need to automatically create answer choices for a question.
    We can use nested attributes instead and create them ourselves.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,6 @@
 class Question < ApplicationRecord
   has_many :answer_choices
 
-  after_create do
-    ["A", "B", "C", "D"].each do |letter|
-      answer_choices.create letter: letter
-    end
   end
 
   def answer
