d05c820b454ebaa119f978a6e9be73fd13c5fe8a
Jared Planter <jaredplanter@gmail.com>
2017-04-12 12:05:30 -0700

    Add QuestionRevision#answer
    To look for an associated AnswerChoice with an answer attribute of true

--- app/models/question_revision.rb
+++ app/models/question_revision.rb
@@ -2,4 +2,8 @@ class QuestionRevision < ApplicationRecord
   has_many :answer_choices, dependent: :destroy
   belongs_to :question
   accepts_nested_attributes_for :answer_choices
+
+  def answer
+    answer_choices.find_by_answer true
+  end
 end
